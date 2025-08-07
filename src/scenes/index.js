9eb4e003e82a4e887281addcfe059eca90d955d2
Marco Machado <marco.machado@gmail.com>
2025-08-07 01:20:07 -0300

    Fix scenes index for named exports

---
+++ src/scenes/index.js
@@ -0,0 +1,5 @@
+export { default as BootScene } from './BootScene.js';
+export { default as MainMenuScene } from './MainMenuScene.js';
+export { default as PreloaderScene } from './PreloaderScene.js';
+export { default as GameScene } from './GameScene.js';
+export { default as UIScene } from './UIScene.js';
